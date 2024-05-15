b8669e6cf9de656f9f02b90dbf8666a075cda992
genis-lorenz <genis.lorenz.otero@alumnat.institutcabanyes.cat>
2024-05-15 19:46:42 +0200

    DataBase accesed

--- wallacabanyes/addetail.py
+++ wallacabanyes/addetail.py
@@ -1,8 +1,15 @@
 from flask import Blueprint
 from markupsafe import escape
+from wallacabanyes.db import get_db
 
 bp = Blueprint('addetail', __name__)
 
 @bp.route('/ad/<id>')
 def detail(id):
-    return f"Detall de l'anunci amb id {escape(id)}"
+    db = get_db()
+
+    ad = db.execute(
+        'SELECT * FROM ad WHERE id = ?', id
+    ).fetchone()
+
+    return f"Detall de l'anunci amb id {escape(id)}\nName: {ad['name']} Subjects: {ad['subjects']} Price: {ad['price']}"
--- wallacabanyes/adlist.py
+++ wallacabanyes/adlist.py
@@ -1,7 +1,14 @@
 from flask import Blueprint
+from wallacabanyes.db import get_db
 
 bp = Blueprint('adlist', __name__)
 
 @bp.route('/ads')
 def adlist():
-    return "Llistat d'anuncis"
+    db = get_db()
+
+    ads = db.execute(
+        'SELECT * FROM ad'
+    ).fetchall()
+
+    return f"Llistat d'anuncis\nId: {ads[0]['id']} Name: {ads[0]['name']} Subjects: {ads[0]['subjects']} Price: {ads[0]['price']}"
