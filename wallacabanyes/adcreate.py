fd0c849bab665d19911244139638b179e56ff7ed
genis-lorenz <genis.lorenz.otero@alumnat.institutcabanyes.cat>
2024-05-20 19:40:45 +0200

    AdCreate Form Implemented

--- wallacabanyes/adcreate.py
+++ wallacabanyes/adcreate.py
@@ -1,7 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, flash
+from wallacabanyes.db import get_db
 
 bp = Blueprint('adcreate', __name__)
 
 @bp.route('/create', methods=('GET', 'POST'))
 def create():
-    return "Creació d'anunci"
+    user_id = 1
+    if request.method == 'GET':
+        cfgm = [ 'Hípica', 'Dinàmica', 'Itineraris', 'Muntanya', 'Bicicleta', 'Lleure', 'Natació', 'SOS', 'Aquàtic', 'Cordes']
+        return render_template('adcreate.html', subjects=cfgm)
+    
+    name = request.form['name']
+    description = request.form['description']
+    size = request.form['size']
+    price = request.form['price']
+    subjects = request.form.getlist('subject_check')
+
+    if not subjects:
+        return render_template('error.html', error='No has seleccionat matèria')
+    
+    db = get_db()
+    db.execute(
+        "INSERT INTO ad (user_id, name, description, size, price, subjects) VALUES (?, ?, ?, ?, ?, ?)",
+        (user_id, name, description, size, price, ','.join(subjects)),
+    )
+
+    return render_template('adlist.html')
