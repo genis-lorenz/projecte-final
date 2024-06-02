0cfbdb9a46ea71083f230142451024f9c37d744b
genis-lorenz <genis.lorenz.otero@alumnat.institutcabanyes.cat>
2024-06-02 16:45:47 +0200

    Added ads for test2

--- wallacabanyes/schema.sql
+++ wallacabanyes/schema.sql
@@ -29,13 +29,45 @@ CREATE TABLE img (
 INSERT INTO user (username, password, grade)
 VALUES ('test@alumnat.institutcabanyes.cat', 'test', 'CFGM');
 
+INSERT INTO user (username, password, grade)
+VALUES ('test2@alumnat.institutcabanyes.cat', 'test2', 'CFGS');
+
 INSERT INTO ad (user_id, name, description, subjects, size, price)
-SELECT user.id, 'Trek Caliber MTB', 'La bici solo tiene 200km','Bicicleta, Muntanya','M', 1099.99
+SELECT user.id, 'Trek Caliber MTB', 'La bici solo tiene 200km','Bicicleta, Muntanya','M', 1099
+FROM user
+WHERE username = 'test@alumnat.institutcabanyes.cat';
+
+INSERT INTO ad (user_id, name, description, subjects, size, price)
+SELECT user.id, 'Aletes', 'Utilitzades una sola vegada','Lleure, Natació','L', 10
 FROM user
 WHERE username = 'test@alumnat.institutcabanyes.cat';
 
 INSERT INTO img (ad_id, filename)
 SELECT ad.id, '1-mtb.jpg'
 FROM ad
-LEFT JOIN user on ad.user_id = user.id
-WHERE username = 'test@alumnat.institutcabanyes.cat';
+WHERE name = 'Trek Caliber MTB'
+
+INSERT INTO img (ad_id, filename)
+SELECT ad.id, '1-aletes.jpg'
+FROM ad
+WHERE name = 'Aletes'
+
+INSERT INTO ad (user_id, name, description, subjects, size, price)
+SELECT user.id, 'Guants de boxe', 'Vermells','Lleure','12oz', 25
+FROM user
+WHERE username = 'test2@alumnat.institutcabanyes.cat';
+
+INSERT INTO ad (user_id, name, description, subjects, size, price)
+SELECT user.id, 'Mancuernas', 'De ferro','Dinàmica','10kg', 30
+FROM user
+WHERE username = 'test2@alumnat.institutcabanyes.cat';
+
+INSERT INTO img (ad_id, filename)
+SELECT ad.id, '1-boxe.jpg'
+FROM ad
+WHERE name = 'Guants de boxe'
+
+INSERT INTO img (ad_id, filename)
+SELECT ad.id, '1-mancuerna.jpg'
+FROM ad
+WHERE name = 'Mancuernas'
